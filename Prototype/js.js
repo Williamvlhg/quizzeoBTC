e382c3e0359e61acdedc043491f574b3ae9de4d5
Williamvlhg <136070530+Williamvlhg@users.noreply.github.com>
2023-06-10 21:21:51 +0200

    Création des fichiers prototypes
    Prototype créé + début de la création de la fonctionnalité du quizz.

---
+++ Prototype/js.js
@@ -0,0 +1,58 @@
+
+var timeLeft = 30;
+
+    var timerId = setInterval(countdown, 1000);
+
+    function countdown() {
+    if (timeLeft == -1) {
+        clearInterval(timerId);
+        document.getElementById("Countdown").innerHTML = "EXPIRED";
+        location.reload();
+    } else {
+        document.getElementById('Countdown').innerHTML = timeLeft;
+        timeLeft--;
+    }
+    }
+
+
+
+
+/*var i;
+
+
+
+setInterval(function(){if (i=31, i>0, i--){document.getElementById("Countdown").innerHTML = "Timer : " + i 
+    }}),1000;
+
+
+
+
+
+    var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+    
+      // Get today's date and time
+      var now = new Date().getTime();
+    
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+    
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+      // Display the result in the element with id="demo"
+      document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+    
+      // If the count down is finished, write some text 
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+    }, 1000);*/
+    
