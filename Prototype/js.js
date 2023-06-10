56c89d9cc83b414822734ad2872ab6fdf3121961
Williamvlhg <136070530+Williamvlhg@users.noreply.github.com>
2023-06-10 21:40:11 +0200

    update prototype
    ajout d'une variable nombre de question

--- Prototype/js.js
+++ Prototype/js.js
@@ -2,17 +2,19 @@
 var timeLeft = 30;
 
     var timerId = setInterval(countdown, 1000);
+    for (var nbq = 20; nbq > 0; nbq = nbq - 1){
+        function countdown() {
+            if (timeLeft == -1) {
+                timeLeft = 30;
+                document.getElementsById("NomQuestion").innerHTML = "Question numéro : " + nbq ;
 
-    function countdown() {
-    if (timeLeft == -1) {
-        clearInterval(timerId);
-        document.getElementById("Countdown").innerHTML = "EXPIRED";
-        location.reload();
-    } else {
-        document.getElementById('Countdown').innerHTML = timeLeft;
-        timeLeft--;
+            } else {
+                document.getElementById('Countdown').innerHTML = timeLeft;
+                timeLeft--;
+            }
+            }
     }
-    }
+    
 
 
 
