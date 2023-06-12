8449850d54ad455cc1adaa9d374d71da0531159c
Veve003 <136071310+Veve003@users.noreply.github.com>
2023-06-12 15:16:02 +0200

    ajout de 2 fichiers php

---
+++ conex.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<img src="c:\Users\esther\Pictures\Image2.png" alt="">
+    <h4>Inscription</h4>
+    <form action="" method="get">
+    
+     <label for="name">Pseudo</label>
+     <input type="name" name="name"><br>
+
+     <label for="pwd">Mot de passe</label>
+     <input type="pwd" name="pwd" ><br>
+
+     
+
+     <input type="submit" value="Valider">
+
+
+    </form>
+  
+
+</body>
+</html>
