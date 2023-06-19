b28a31105daf5cf2fc09d23a2aa23889c6e0e5f9
Williamvlhg <136070530+Williamvlhg@users.noreply.github.com>
2023-06-19 15:53:44 +0200

    finition form inscription

--- PrototypeW/PageInscription/index.php
+++ PrototypeW/PageInscription/index.php
@@ -11,13 +11,16 @@
     <form action="" method="post">
     
      <label for="name">Pseudo</label>
-     <input type="text" name="name"><br>
+     <input type="text" name="name" required><br>
 
      <label for="email">E-mail</label>
-     <input type="email" name="email"><br>
+     <input type="email" name="email" required><br>
 
      <label for="pwd">Mot de passe</label>
-     <input type="password" name="pwd" ><br>
+     <input type="password" name="pwd" required><br>
+
+     <label for="pwd2">Comfirmer votre Mot de passe</label>
+     <input type="password" name="pwd2" required><br>
 
      <input type="submit" name="submit" value="Valider">
 
--- PrototypeW/PageInscription/processusinscription.php
+++ PrototypeW/PageInscription/processusinscription.php
@@ -6,14 +6,18 @@
             $u = $_POST["name"];
             $e = $_POST["email"];
             $p = $_POST["pwd"];
-            $p = password_hash($p, PASSWORD_DEFAULT);
-
+            $p2 = $_POST["pwd2"];
+        if($p2 != $p){
+            echo "Vos mots de passes ne correspondent pas";
+        } else {
+        $p = password_hash($p, PASSWORD_DEFAULT); 
         $sql = $db->prepare("INSERT INTO User(Username, Email, mdp, Role) VALUES ( :Username, :Email, :mdp, 1);");
         $sql -> execute([
             'Username' => $u,
             'Email' => $e,
             'mdp' => $p
         ]);
+        }
     }
 
     
