66ed720ff5beedd2e7d51f849dae4b4a98c376c9
Williamvlhg <136070530+Williamvlhg@users.noreply.github.com>
2023-07-07 17:09:24 +0200

    Delete zoomzoom directory

--- zoomzoom/PageListing/debutjeu2.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    $idquizz = 2;
-    header("location: ../PageJeu/Prototype/test.php");
-    ?>
--- zoomzoom/PageListing/debutjeu4.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    $idquizz = 4;
-    header("location: ../PageJeu/Prototype/test.php");
-    ?>
